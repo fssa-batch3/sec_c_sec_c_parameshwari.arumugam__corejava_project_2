d046d6c8ef0a9ca79fec92c513b148264ac397d6
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-08-16 22:39:15 +0530

    commit

--- src/main/java/com/fssa/flowerybouquet/service/ProductService.java
+++ src/main/java/com/fssa/flowerybouquet/service/ProductService.java
@@ -8,13 +8,7 @@ import com.fssa.flowerybouquet.model.Product;
 import com.fssa.flowerybouquet.validator.ProductValidator;
 
 public class ProductService {
-//	private static ProductDao productDao;
-
-//	public void setDependencies(ProductValidator validator, ProductDao dao) {
-//		productValidator = validator;
-//		ProductDao = dao; 
-//	}  
-
+//	
 	public boolean addProduct(Product product) throws DAOException, SQLException {
 		if (ProductValidator.validate(product)) {
 			ProductDao productDao = new ProductDao();
@@ -39,7 +33,7 @@ public class ProductService {
 		return true;
 	}
 
-	public boolean getAllProductDetails() throws DAOException, SQLException {
+	public boolean getAllProductDetails() throws SQLException {
 		ProductDao productDao = new ProductDao();
 		productDao.getAllProductDetails();
 		return true;
